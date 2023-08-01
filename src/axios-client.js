2138cbd9d5f7a4c7ddd470314f7e4e672bb7c957
Alfin Febrian <20k10020@student.unika.ac.id>
2023-08-01 19:57:59 +0700

    feat(axios-client): redo add withCredentials

--- src/axios-client.js
+++ src/axios-client.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
-    // withCredentials: true,
+    withCredentials: true,
 })
 
 axiosClient.interceptors.request.use((config) => {
